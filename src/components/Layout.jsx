6cf1124ff4adf3c2f2622371c1bd0fbd3d5426b9
jonathan <Jmerol27@gmail.com>
2023-06-25 18:31:56 -0600

    inicio

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom"
+
+const Layout = () => {
+  return (
+    <div>
+        <h1 className="text-6xl font-bold">CRM React</h1>
+    </div>
+  )
+}
+
+export default Layout
